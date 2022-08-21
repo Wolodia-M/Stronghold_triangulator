4818e3612396d1039eb3388f1215944aad057db1
Wolodia-M <104127709+Wolodia-M@users.noreply.github.com>
2022-08-21 19:44:42 +0300

    Update stronghold_triangulator.hpp

--- src/stronghold_triangulator.hpp
+++ src/stronghold_triangulator.hpp
@@ -1,8 +1,24 @@
+// Stronghold triangulator - App for simplify procces of finding strongholds
+// Copyright (C) 2022  WolodiaM
+
+// This app is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This app is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this app; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 /*
  *	Stronghold triangulator
- *  App for easier way to locate stronghold in Minecraft
+ *  App for easier way to locate stronghold
  *  App use triangulation metod to do this
- *  !!!!This is console app!!!!
+ *  !!!!This is terminal app!!!!
  *
  *	Created by WolodiaM
  */
