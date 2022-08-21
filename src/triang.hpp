1f1e20807c3a6e58da14aab7feb38ccdf78fe01e
Wolodia-M <104127709+Wolodia-M@users.noreply.github.com>
2022-08-21 19:41:38 +0300

    Update triang.hpp

--- src/triang.hpp
+++ src/triang.hpp
@@ -1,3 +1,19 @@
+// Stronghold triangulator - App for simplify procces of finding strongholds
+// Copyright (C) 2022  WolodiaM
+
+// This app is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This app is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this app; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 namespace triang
 {
     void getcord(int x1, int z1, float f1, int x2, int z2, float f2);
